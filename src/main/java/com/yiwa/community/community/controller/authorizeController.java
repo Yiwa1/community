ad4fc24f04ed1d64758b1aef3e94055c486eb54c
Yiwa1 <2280833484@qq.com>
2021-09-15 00:07:07 +0800

    修改配置文件

--- src/main/java/com/yiwa/community/community/controller/authorizeController.java
+++ src/main/java/com/yiwa/community/community/controller/authorizeController.java
@@ -4,6 +4,7 @@ import com.yiwa.community.community.dto.AccessTokenDTO;
 import com.yiwa.community.community.dto.GitHubUser;
 import com.yiwa.community.community.provider.GitHubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,17 +16,20 @@ public class authorizeController {
     @Autowired
     GitHubProvider gitHubProvide;
 
+    @Value("${github.client.id}")
+    private String client_id;
+    @Value("${github.client.secret}")
+    private String client_secret;
+
     @RequestMapping("/callback")
     public String callback(@RequestParam(name = "code")String code){
         //设置access_token
-        accessToken.setClient_id("723ca7c1206fcfbff6cb");
+        accessToken.setClient_id(client_id);
         accessToken.setCode(code);
-        accessToken.setClient_secret("1d95bd7386cece906dad5112cd3ef0a7a63d550a");
+        accessToken.setClient_secret(client_secret);
         String token= gitHubProvide.getAccessToken(accessToken);
         GitHubUser user = gitHubProvide.getUser(token);
         System.out.println(user);
-
-
         return "index";
 
     }
--- src/test/java/com/yiwa/community/community/CommunityApplicationTests.java
+++ src/test/java/com/yiwa/community/community/CommunityApplicationTests.java
@@ -1,13 +1,23 @@
 package com.yiwa.community.community;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CommunityApplicationTests {
 
+    @Value("${github.client.id}")
+    private String client_id;
+    @Value("${github.client.secret}")
+    private String client_secret;
+
     @Test
     void contextLoads() {
+        System.out.println(client_id);
+        System.out.println(client_secret);
     }
 
 }
