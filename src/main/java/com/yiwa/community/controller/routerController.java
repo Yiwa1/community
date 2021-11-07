f2f317049affdc60f186eb46f129a6d526000909
Yiwa1 <2280833484@qq.com>
2021-11-07 15:10:34 +0800

    增加搜索功能并追加日志

--- src/main/java/com/yiwa/community/controller/routerController.java
+++ src/main/java/com/yiwa/community/controller/routerController.java
@@ -6,16 +6,20 @@ import com.yiwa.community.dao.UserMapper;
 import com.yiwa.community.dto.PaginationDTO;
 import com.yiwa.community.dto.QuestionDTO;
 import com.yiwa.community.pojo.User;
-import com.yiwa.community.service.NotificationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.thymeleaf.util.StringUtils;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -24,6 +28,7 @@ import java.util.List;
  * @version 1.0
  * @Date: 2021/9/14
  * */
+
 
 @Controller
 public class routerController{
@@ -37,11 +42,14 @@ public class routerController{
     @Autowired
     NotificationMapper notificationMapper;
 
+    private static Logger logger= LoggerFactory.getLogger(routerController.class);
+
     @GetMapping("/")
     public String index(HttpServletRequest request,
                         Model model,
                         @RequestParam(name = "page",defaultValue = "1") Integer page,
-                        @RequestParam(name = "pageSize",defaultValue = "15") Integer pageSize){
+                        @RequestParam(name = "pageSize",defaultValue = "15") Integer pageSize,
+                        @RequestParam(name="search",defaultValue = "")String search){
 
 
         /*
@@ -56,6 +64,8 @@ public class routerController{
                   String token = cookie.getValue();
                   User user = userMapper.queryUserByToken(token);
                   if (user != null) {
+                      //回头客
+                      logger.info(user.getAccountId()+" 用户再次登录");
                       //将用户信息存入session中，用于页面展示信息
                       /*
                       *********************************
@@ -73,30 +83,58 @@ public class routerController{
           }
       }
 
-        //查看所有问题数量
-        Integer totalCount=questionMapper.count();
+      if(!StringUtils.isEmpty(search)){
+          String[] split = search.split(" ");
+          search = Arrays.stream(split).collect(Collectors.joining("|"));
+          //查看搜索问题结果数量
+          Integer totalCount=questionMapper.countSearchResult(search);
 
-      //注意page异常输入
+          //注意page异常输入
 
-        if(page<1){
-            page=1;
-        }else if(page!=1&&page>(int)Math.ceil((double)totalCount/pageSize)){
-            page=(int)Math.ceil((double)totalCount/pageSize);
-        }
+          if(page<1){
+              page=1;
+          }else if(page!=1&&page>(int)Math.ceil((double)totalCount/pageSize)){
+              page=(int)Math.ceil((double)totalCount/pageSize);
+          }
 
-        //设置用于分页的基本参数
-        PaginationDTO paginationDTO=new PaginationDTO();
-        paginationDTO.setPage(page);
-        paginationDTO.setPageSize(pageSize);
-        paginationDTO.setTotalCount(totalCount);
-        paginationDTO.setPagination();
-        //计算偏移量
-        Integer offset=(page-1)*pageSize;
-        //分页查询
-        List<QuestionDTO> questions = questionMapper.queryAllQuestion(offset,pageSize);
-        model.addAttribute("questions",questions);
-        model.addAttribute("pagination",paginationDTO);
+          //设置用于分页的基本参数
+          PaginationDTO paginationDTO=new PaginationDTO();
+          paginationDTO.setPage(page);
+          paginationDTO.setPageSize(pageSize);
+          paginationDTO.setTotalCount(totalCount);
+          paginationDTO.setPagination();
+          //计算偏移量
+          Integer offset=(page-1)*pageSize;
+          //分页查询
+          List<QuestionDTO> questions = questionMapper.queryQuestionByKeyWord(search,offset,pageSize);
+          model.addAttribute("questions",questions);
+          model.addAttribute("pagination",paginationDTO);
+          model.addAttribute("search",search);
+      }else {
 
+          //查看所有问题数量
+          Integer totalCount = questionMapper.count();
+
+          //注意page异常输入
+
+          if (page < 1) {
+              page = 1;
+          } else if (page != 1 && page > (int) Math.ceil((double) totalCount / pageSize)) {
+              page = (int) Math.ceil((double) totalCount / pageSize);
+          }
+          //设置用于分页的基本参数
+          PaginationDTO paginationDTO = new PaginationDTO();
+          paginationDTO.setPage(page);
+          paginationDTO.setPageSize(pageSize);
+          paginationDTO.setTotalCount(totalCount);
+          paginationDTO.setPagination();
+          //计算偏移量
+          Integer offset = (page - 1) * pageSize;
+          //分页查询
+          List<QuestionDTO> questions = questionMapper.queryAllQuestion(offset, pageSize);
+          model.addAttribute("questions", questions);
+          model.addAttribute("pagination", paginationDTO);
+      }
         return "index";
     }
 
